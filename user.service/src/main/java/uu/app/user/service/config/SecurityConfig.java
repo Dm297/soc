f479f3dd522c83fe6cbddfa96ea3333588a19a07
Dmytro <blazh-dima@ukr.net>
2025-05-25 00:26:11 +0300

    Lab 6

--- user.service/src/main/java/uu/app/user/service/config/SecurityConfig.java
+++ user.service/src/main/java/uu/app/user/service/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.web.SecurityFilterChain;
 import uu.app.user.service.filter.JwtAuthenticationFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 
 
 @Configuration
@@ -22,6 +23,7 @@ public class SecurityConfig {
             Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
--- user.service/src/main/java/uu/app/user/service/controllers/UserController.java
+++ user.service/src/main/java/uu/app/user/service/controllers/UserController.java
@@ -7,9 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
     private final UserService userService;
 
@@ -19,17 +21,20 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity user) {
+        log.info("Create User {}", user);
         return ResponseEntity.ok(userService.createUser(user));
     }
 
     @GetMapping
     public ResponseEntity<List<UserEntity>> getAllUsers() {
+        log.info("Get All Users");
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserEntity> getUserById(@PathVariable Long id) {
         Optional<UserEntity> user = userService.getUserById(id);
+        log.info("Get User {}", user);
         return user.map(ResponseEntity::ok).orElseGet(() ->
                 ResponseEntity.notFound().build());
     }
@@ -37,6 +42,7 @@ public class UserController {
     public ResponseEntity<UserEntity> updateUser(@PathVariable Long id,
                                                  @RequestBody UserEntity user) {
         UserEntity updatedUser = userService.updateUser(id, user);
+        log.info("Update User {}", user);
         return updatedUser != null ? ResponseEntity.ok(updatedUser) :
                 ResponseEntity.notFound().build();
     }
@@ -44,6 +50,7 @@ public class UserController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
+        log.info("Delete User with Id {}", id);
         return ResponseEntity.noContent().build();
     }
 }
